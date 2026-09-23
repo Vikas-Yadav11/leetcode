790fb741bc5a972dfd9f24893981f96863ca6d30
Vikas-Yadav11 <vy6160573@gmail.com>
2026-09-23 15:36:47 +0530

    Time: 5 ms (45.08%), Space: 43.3 MB (26.96%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.java
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -1,5 +1,5 @@
 class Pair{
-     char ch;
+    char ch;
     int freq;
     Pair(char c,int f){
         ch=c;
@@ -8,30 +8,30 @@ class Pair{
 }
 class Solution {
     public String reorganizeString(String s) {
-        HashMap<Character,Integer> map=new HashMap<>();
+        HashMap<Character,Integer> map = new HashMap<>();
         for(int i=0;i<s.length();i++){
         char ch=s.charAt(i);
         map.put(ch,map.getOrDefault(ch,0)+1);
-    }
-    PriorityQueue<Pair>pq =new PriorityQueue<>((a,b)->b.freq-a.freq);
-    for(char ch:map.keySet()){
-        Pair p=new Pair(ch,map.get(ch));
-        pq.add(p);
-    }
-    StringBuilder ans=new StringBuilder();
-    Pair previous = null;
-    while(!pq.isEmpty()){
-         Pair current=pq.poll();
-        ans.append(current.ch);
-        current.freq--;
-        if(previous!=null && previous.freq>0){
-            pq.add(previous);
         }
-        previous=current;
+        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->b.freq-a.freq);
+        for(char ch:map.keySet()){
+            Pair current=new Pair(ch,map.get(ch));
+            pq.add(current);
+        }
+        StringBuilder ans=new StringBuilder();
+        Pair previous=null;
+        while(!pq.isEmpty()){
+            Pair current=pq.poll();
+            ans.append(current.ch);
+            current.freq--;
+            if(previous!=null && previous.freq>0){
+                pq.add(previous);
+            }
+            previous=current;
+        }
+        if(ans.length()!=s.length()){
+            return "";
+        }
+return ans.toString();
     }
-    if(s.length()!=ans.length()){
-        return "";    
-    }
-    return ans.toString();
-}
 }
